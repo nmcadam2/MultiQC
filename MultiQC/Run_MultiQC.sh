a75f3e71c730ef364d55d11f24b42bf383b540fa
nmcadam2 <nmcadam2@nd.edu>
2024-02-14 23:17:11 -0500

    Updated
    Script now has singularity pull the Multiqc docker container.  This also adds some more information about the multiqc version.
    
    README updated to include my directory structure when I made this for troubleshooting purposes.

---
+++ MultiQC/Run_MultiQC.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+#$ -M nmcadam2@nd.edu
+#$ -m abe
+#$ -q debug
+#$ -N MultiQC-test
+
+DIRECTORY=$(pwd)
+TOOL=multiqc
+INPUT=$DIRECTORY/input_data/
+VERSION=1.20
+
+#Singularity Check
+if [ ! $(command -v singularity) ]; then
+        module load singularity
+	echo "loaded singularity"
+fi
+#Fastqc check
+if [ ! $(command -v fastqc) ]; then
+        module load bio/2.0
+	echo "loaded bio/2.0 module"
+fi
+
+#make output folder
+if [ -d "fastqc" ]; then
+    echo "./fastqc exists"
+else
+    mkdir fastqc
+fi
+
+#run fastqc
+for g in $INPUT/*.gz; do
+	fastqc -o ./fastqc $g
+done
+echo "fastqc complete"
+
+#container install
+if [ -d "image" ]; then
+    echo "./image exists"
+else
+    mkdir image
+fi
+
+singularity pull docker://multiqc/multiqc:v1.20
+mv *.sif $DIRECTORY/image/$TOOL-v$VERSION.sif
+
+#run multiqc
+FASTQC=$DIRECTORY/fastqc/
+singularity exec -B $DIRECTORY $DIRECTORY/image/$TOOL-v$VERSION.sif $TOOL $FASTQC
